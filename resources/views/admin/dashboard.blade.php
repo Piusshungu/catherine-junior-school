5700075cdb39685f8f14ac9a9d47adf2ccdf97c2
Piusshungu <shungupius@gmail.com>
2022-01-18 16:22:04 +0300

    Added Admin Controller for operation management

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-sidebar />
+
+<x-manage-students />
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\AdminController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +24,5 @@ Route::get('/', function () {
 Route::get('/' , [HomeController::class, 'index']);
 
 Route::get('/dashboard', [StudentsController::class, 'index']);
+
+Route::get('/dashboard', [AdminController::class, 'index']);
