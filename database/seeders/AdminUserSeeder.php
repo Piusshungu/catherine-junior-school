a7f6866d22f729f88ee15aa8636f44a687cbc257
Piusshungu <shungupius@gmail.com>
2022-02-10 10:38:48 +0300

    Seeded an admin user with login credential

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Shungu Pius', 
+            'email' => 'shungupius@gmail.com',
+            'password' => bcrypt('shungu')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
