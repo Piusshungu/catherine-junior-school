0a676a6f312c073b6dda5ed9eed78b4090f17c9d
Piusshungu <shungupius@gmail.com>
2022-02-14 11:21:50 +0300

    Retaining a search value after showing results

--- resources/views/admin/manage-students.blade.php
+++ resources/views/admin/manage-students.blade.php
@@ -26,7 +26,7 @@
                             <input type="hidden" name="category" value="{{ request('category') }}">
                             @endif
 
-                            <input type="search" name="search" class="rounded-full form-control relative flex-auto min-w-0 block w-80 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2">
+                            <input type="search" value="{{ request('search') }}" name="search" class="rounded-full form-control relative flex-auto min-w-0 block w-80 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2">
 
                         </form>
                     </div>
