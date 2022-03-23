c566e49c90e03c80124f047622dca262f7a3d1c9
Piusshungu <shungupius@gmail.com>
2022-03-23 18:06:48 +0300

    Changed the dimensions of the contents in the footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -107,38 +107,30 @@
 
 
 
-            <div class="grid lg:grid-cols-4 md:grid-cols-4">
-                <div class="mb-6 col">
-                    <h5 class="uppercase font-bold mb-2.5">Contacts</h5>
+            <div class="grid lg:grid-cols-4 md:grid-cols-4 ml-36 mt-12">
+
+                <div class="mb-6 col ml-0">
+                    <h5 class="uppercase font-bold mb-2.5 ml-0">Updates</h5>
 
                     <ul class="list-none mb-0">
-
-                    <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
                             </svg>:
-                            <a href="#!" class="text-white">shungupius@gmail.com</a>
+                            <a href="#!" class="text-white">Events</a>
                         </li><br>
-
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                             </svg>:
-
-                            <a href="#!" class="text-white">+255714262024</a>
-
+                            <a href="#!" class="text-white">Results</a>
                         </li><br>
-
-                     
-
                         <li>
-
-                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>:
-                            <a href="#!" class="text-white">P.O Box 1078 DSM</a>
+                            <a href="#!" class="text-white">Gallery</a>
                         </li>
-
                     </ul>
                 </div>
 
@@ -210,7 +202,7 @@
                     </ul>
                 </div>
 
-                
+
 
 
             </div>
