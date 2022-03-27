742c1f33f8181239e2b1489d204f763fe5036c47
Piusshungu <shungupius@gmail.com>
2022-03-27 10:37:01 +0300

    Added a page to send Email notifications to individual parent

--- app/Mail/NotificationToParents.php
+++ app/Mail/NotificationToParents.php
@@ -34,6 +34,6 @@ class NotificationToParents extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.individual-parent');
     }
 }
