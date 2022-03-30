137664b5eefcc48e8fc12be50d93fe37c102e6a7
Piusshungu <shungupius@gmail.com>
2022-03-30 17:42:34 +0300

    Passed classes and users in class index screen

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Level;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ClassesController extends Controller
 {
     public function index()
     {
-        return view('classes.index');
+        return view('classes.index',[
+
+            'users' => User::all(),
+
+            'classes' => Level::all(),
+        ]);
     }
 }
