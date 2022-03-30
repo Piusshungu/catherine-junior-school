b08b5be1ca054f2e2237e01e17e75a7dfbc0fef9
Piusshungu <shungupius@gmail.com>
2022-03-30 14:47:36 +0300

    Added a link to classes page on sidebar

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ClassesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('classes.index');
+    }
 }
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -87,7 +87,7 @@
                     </li>
 
                     <li class="mb-2 rounded hover:shadow hover:bg-gray-800">
-                        <a href="/students" class="inline-block w-full h-full px-3 py-2 font-bold text-white">
+                        <a href="/classes" class="inline-block w-full h-full px-3 py-2 font-bold text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path d="M12 14l9-5-9-5-9 5 9 5z" />
                                 <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
