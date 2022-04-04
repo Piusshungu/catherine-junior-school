00259c27e860c03b84277f3c76ba4afe5d9d9e92
Piusshungu <shungupius@gmail.com>
2022-04-04 14:57:23 +0300

    Added a notification menu on side-bar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -32,33 +32,9 @@
             <div class="hidden lg:block">
                 <div class="my-2 mb-6">
 
-                    <!-- <ul>
-
-                        <li class="mb-2 rounded hover:shadow">
-                            <a href="/dashboard" class="inline-block w-full h-full px-3 py-2 font-bold text-white">
-                                <svg class="inline-block h-10 w-10 text-blue-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-
-                                Welcome, {{ auth()->user()->name }}
-                            </a>
-                        </li>
-
-                    </ul> -->
-
                 </div>
                 <ul>
-                    <!-- <li class="mb-6">
-                        <div class="relative">
-                            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                                <button type="submit" class="p-1 focus:outline-none">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-4 h-4">
-                                        <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                    </svg>
-                                </button>
-                            </span>
-                            <input type="search" name="search" class="w-full px-4 py-2 pl-12 rounded shadow outline-none" placeholder="Search...">
-                    </li> -->
+                
                     <li class="mb-2 rounded hover:shadow hover:bg-gray-800">
                         <a href="/dashboard" class="inline-block w-full h-full px-3 py-2 font-bold text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 mr-2 -mt-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -106,6 +82,25 @@
                         </a>
                     </li>
 
+                        <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                            <button @click="open = !open" class="flex flex-row items-center w-full px-2 py-2 mt-4 text-sm font-semibold text-left bg-transparent rounded-lg text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:bg-gray-800 hover:text-white focus:text-gray-800 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 mr-2 -mt-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                                </svg>
+
+                                <span>Notifications</span>
+                                <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-8 transition-transform duration-200 transform md:-mt-1">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                            <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full h-full mt-2 origin-top-right rounded-md shadow-lg">
+                                <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+                                    <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Teachers</a>
+                                    <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Parents</a>
+                                </div>
+                            </div>
+                        </div><br>
+
                     <li class="mb-2 rounded hover:shadow hover:bg-gray-800">
                         <a href="/payment" class="inline-block w-full h-full px-3 py-2 font-bold text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -143,6 +138,8 @@
                             Settings
                         </a>
                     </li>
+
+
 
                     <li class="mb-2 rounded hover:shadow hover:bg-gray-800">
                         <a href="/logout" class="inline-block w-full h-full px-3 py-2 font-bold text-white">
