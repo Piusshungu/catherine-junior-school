59c85f6ee4088947e965e83075fd09d16c78ec59
Piusshungu <shungupius@gmail.com>
2022-04-12 16:23:14 +0300

    Added a foreign key in students table

---
+++ database/migrations/2022_04_12_084507_add_level_id_to_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Emadadly\LaravelUuid\Uuids;
+
+class AddLevelIdToStudentsTable extends Migration
+{
+
+    use Uuids;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+
+            $table->foreignUuid('level_id')->nullable()->references('id')->on('levels')->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+}
