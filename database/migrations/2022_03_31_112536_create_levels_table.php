cebd973a75120af3a5303c6324f5893e1ef9fa12
Piusshungu <shungupius@gmail.com>
2022-04-12 16:20:23 +0300

    Added a primary key to levels table

---
+++ database/migrations/2022_03_31_112536_create_levels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Emadadly\LaravelUuid\Uuids;
+
+class CreateLevelsTable extends Migration
+{
+
+    use Uuids;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('levels', function (Blueprint $table) {
+                $table->uuid('id');
+                $table->foreignUuid('user_id')->references('id')->on('users')->cascadeOnUpdate();
+                $table->string('class');
+                $table->string('stream');
+                $table->timestamps();
+            });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('levels');
+    }
+}
