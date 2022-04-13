f14bdfe891035a5de4277efaeb7e38aedf44362b
Piusshungu <shungupius@gmail.com>
2022-04-13 17:41:27 +0300

    Added uuid validation on migration files and Attendance model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Emadadly\LaravelUuid\Uuids;
 
 class Attendance extends Model
 {
     use HasFactory;
+
+    use Uuids;
+
+    public $incrementing = false;
+
+    protected $guarded = ['id'];
 }
