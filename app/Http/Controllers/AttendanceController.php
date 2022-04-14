4febe22855343192b3e834d03271a2b8ebbc0ef1
Piusshungu <shungupius@gmail.com>
2022-04-14 09:41:46 +0300

    Added a method to land all list of students in attendance page

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    //
+    public function index(){
+
+        return view('attendance',[
+            
+            'students' => Student::orderBy('first_name')->filter(request(['search']))
+
+            ->paginate(7)->withQueryString(),
+        ]);
+    }
 }
