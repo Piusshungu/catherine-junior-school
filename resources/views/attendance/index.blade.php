c35f5705355755a273979d158ce04d911fd68ed2
Piusshungu <shungupius@gmail.com>
2022-04-19 08:59:58 +0300

    Refactoring the atendance screen for permitted buttons

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -173,9 +173,7 @@
 
                                             <input type="hidden" name="attendences[{{$loop->index}}][student_id]" value="{{$student->id}}">
                                             <!-- line -->
-                                            <div class="block bg-gray-5
-                                            
-                                            00 w-14 h-8 rounded-full"></div>
+                                            <div class="block bg-gray-500 w-14 h-8 rounded-full"></div>
                                             <!-- dot -->
                                             <div class="permitted absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
                                         </div>
