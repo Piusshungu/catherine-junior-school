5ba3376ff8c3af5099d884c4be66e2dddea40879
Piusshungu <shungupius@gmail.com>
2022-04-29 11:25:09 +0300

    Added column attributes in subjects migration

--- database/migrations/2022_04_29_080910_create_subjects_table.php
+++ database/migrations/2022_04_29_080910_create_subjects_table.php
@@ -3,9 +3,12 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Emadadly\LaravelUuid\Uuids;
 
 class CreateSubjectsTable extends Migration
 {
+
+    use Uuids;
     /**
      * Run the migrations.
      *
@@ -14,7 +17,9 @@ class CreateSubjectsTable extends Migration
     public function up()
     {
         Schema::create('subjects', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('subject_name');
             $table->timestamps();
         });
     }
