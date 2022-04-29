5fd42fd01031166e6bb71d9789fc66c9cc5d0d4d
Piusshungu <shungupius@gmail.com>
2022-04-29 12:21:26 +0300

    Added a method to save subjects records

--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -14,4 +14,24 @@ class SubjectsController extends Controller
             'subjects' => Subject::all(),
         ]);
     }
+
+    public function createSubject()
+    {
+        $classData = request()->validate([
+
+            'name' => 'required',
+        ]);
+
+        $classData = Subject::create($classData);
+
+        if(request()->levels){
+
+            foreach(request()->levels as $level){
+
+                $classData->levels()->attach($level);
+            }
+        }
+
+        return redirect('/levels')->with('success', 'Subject records successfully added');
+    }
 }
