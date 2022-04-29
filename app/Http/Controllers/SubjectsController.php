4762456ab978ae0d04c3dd7c9e9aabaf5be45b66
Piusshungu <shungupius@gmail.com>
2022-04-29 12:00:54 +0300

    Added a method to view all available subjects

--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SubjectsController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('subjects.index', [
+
+            'subjects' => Subject::all(),
+        ]);
+    }
 }
