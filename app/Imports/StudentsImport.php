16350a785cbebfeeabd3a057692297f80b8ce119
Piusshungu <shungupius@gmail.com>
2022-05-12 09:45:55 +0300

    Modified the column in excel file to be imported

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -4,12 +4,17 @@ namespace App\Imports;
 
 use App\Models\Student;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class StudentsImport implements ToModel, WithHeadingRow
+class StudentsImport implements ToModel, WithHeadingRow, WithValidation
 {
+
+    use Importable;
+
     public function model(array $row)
     {
         return new Student([
@@ -17,11 +22,25 @@ class StudentsImport implements ToModel, WithHeadingRow
             'full_name' => $row['full_name'],
             'last_name' => $row['last_name'],
             'registration_number' => $row['registration_number'],
+            'registration_year' => $row['registration_year'],
             'dob' => $row['dob'],
             'gender' => $row['gender'],
             'phone_number' => $row['phone_number'],
             
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+
+            '0' => 'required|string',
+            '1' => 'required|string',
+            '2' => 'required',
+            '3' => 'required',
+            '4' => 'required',
+            '5' => 'required'
+        ]
     }
 
     // public function chunkSize(): int
