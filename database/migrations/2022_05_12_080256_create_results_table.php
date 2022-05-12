07d0cb400c3a38666aa9bc69521fd9f47d5ee704
Piusshungu <shungupius@gmail.com>
2022-05-12 11:21:28 +0300

    Added a migration file for results with respective model

---
+++ database/migrations/2022_05_12_080256_create_results_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Emadadly\LaravelUuid\Uuids;
+
+class CreateResultsTable extends Migration
+{
+
+    use Uuids;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('subject');
+            $table->foreignUuid('student_id')->references('id')->on('students')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->decimal('score', 6,2);
+            $table->string('class');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
